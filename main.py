08ce1ea62d92378083b727a08f8c0acda4d03574
Harold-coder <hjc2154@columbia.edu>
2023-10-22 17:07:08 -0400

    Added Hello World app

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Response
+
+# I like to launch directly and not use the standard FastAPI startup
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World. This is Order Management Service!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8012)
