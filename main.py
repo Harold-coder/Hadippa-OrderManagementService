0c1ea8a59606edd44f0452f7ee7c2d677d90406e
Harold-coder <hjc2154@columbia.edu>
2023-10-31 19:20:29 -0400

    Tutorial

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from flask import Flask
-import os
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World. This is Order Management Service!"
+
+@app.route("/")
+def hello() -> str:
+    """Return a friendly HTTP greeting.
+
+    Returns:
+        A string with the words 'Hello World!'.
+    """
+    return "Hello World!"
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app.
+    app.run(host="127.0.0.1", port=8080, debug=True)
